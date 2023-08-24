138a0be134e75b8155bd2b7f82ad47a368022ccd
CUBOVR <cristobalurquidesb@gmail.com>
2023-08-24 13:56:19 -0600

    Third commit

--- tests/busqueda.spec.js
+++ tests/busqueda.spec.js
@@ -5,29 +5,29 @@ test('busqueda', async({page})=>{
     //dirige a la dirección URL
     await page.goto("https://www.mercadolibre.com/")
 
-    //click on MX country button
+    //click on MX country button 
     await page.locator('id=MX').click()
-    await page.screenshot({ pat: 'Inicio.png'})
+    await page.screenshot({ path: 'test-results\Inicio.png'})
 
     //provide the item
     await page.locator('id=cb1-edit').fill("playstation 5")
-    await page.screenshot({ pat: 'Item-Search.png'})
+    await page.screenshot({ path: 'test-results\Item-Search.png'})
 
     //Click search 
     await page.locator('//div/div[2]/form/button/div').click()
     
     //Filter by conditions
     await page.locator('//div[2]/aside/section/div[7]/ul/li[1]/a/span[1]').click()
-    await page.screenshot({ pat: 'Condicion.png'})
+    await page.screenshot({ path: 'test-results\Condicion.png'})
 
     //Filter by location
     await page.locator('//ul/li/a/span[text()="Distrito Federal"]').click()
-    await page.screenshot({ pat: 'FiltroUbicacion.png'})
+    await page.screenshot({ path: 'test-results\FiltroUbicacion.png'})
 
     //Order by price
     await page.click('//*[@id=":Raokpp:-trigger"]')
     await page.click('//*[@id=":Raokpp:-menu-list-option-price_desc"]/div/div/span')
-    await page.screenshot({ pat: 'Mayor-menor.png'})
+    await page.screenshot({ path: 'test-results\Mayor-menor.png'})
 
     //completo
     const enlaces = await page.evaluate(() => {
